bc9865a0d0838d3dcd856577c11d65ed2936e74e
Thomas Fazziani <thomasfazziani@gmail.com>
2023-03-13 16:59:21 +0100

    Esercizi finiti

---
+++ script.js
@@ -0,0 +1,73 @@
+// let numberOfShots = 3
+// let player1 = 0
+// let player2 = 0
+
+// for(let i = 0; i <= numberOfShots; i++) {
+//     player1 +=   Math.floor(Math.random() * (6 - 1) + 1);
+//     player2 +=   Math.floor(Math.random() * (6 - 1) + 1);
+// }
+// if (player1 > player2) {
+//     console.log(`Punteggio totale del player1 è di ${player1} punti`);
+// } else if( player1 < player2)
+//     console.log(`Punteggio totale del player2 è di ${player2}`);
+// es 2
+  
+// function calcmult (num) 
+// {
+//     for (let i = 1; i <= num; i++){
+//         if (i % 15 == 0 ){
+//             console.log('Fizzbuzz');
+//         }
+//         else if (i % 3 == 0) {
+//           console.log('Fizz');
+//         }
+//         else if (i % 5 == 0 ){
+//             console.log('Buzz');
+//         }
+      
+//         else console.log(i);
+//     }
+   
+//   }
+
+//  calcmult(30);
+
+//  es 3
+// let numbers = [10, 12, 78, -4, -20, 11];
+// function maggiore () {
+//     console.log(Math.max(...numbers));
+// }
+// function minore () {
+//     console.log(Math.min(...numbers))
+// }
+// minore()
+// maggiore()
+// es 4
+//  let array = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4]
+//  function decrescente() {
+//      console.log( array.sort().reverse() );
+
+//  }
+//  decrescente()
+//  function crescente() {
+//     console.log( array.sort());
+
+// }
+// crescente()
+// es 5
+// function contatore() {
+//     let num = 100
+    
+//     let ncifre = 0
+//     if (num < 10 ) {
+//         ncifre = 1
+//     }
+//     else if (num < 100) {
+//         ncifre = 2
+//     } 
+//     else if (num < 1000) {
+//         ncifre = 3
+//     }  
+//     return console.log(ncifre);
+// }
+// contatore()
